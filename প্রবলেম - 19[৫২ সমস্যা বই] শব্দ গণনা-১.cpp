e6dbed2661c3693449bf11380b9822cf1758185b
Sree Prosenzit Kumar Sharma <79397907+jeet-sharma-bd@users.noreply.github.com>
2021-03-13 01:03:01 -0800

    প্রবলেম - 19 [৫২ সমস্যা বই] শব্দ গণনা-১
    একটি বাক্যে অবস্থিত শব্দগুলোর মধ্যকার স্পেস হিসাব করে মোট শব্দ কতগুলো আছে সেটি বের করার প্রোগ্রাম লিখতে হবে।
    
    ইনপুট
    
    প্রোগ্রামটির প্রথম ইনপুট হবে একটি পূর্ণসংখ্যা T, যা টেস্ট কেসের সংখ্যা নির্দেশ করে। পরবর্তী T সংখ্যক লাইনে ইনপুট হবে একটি স্ট্রিং S। যেখানে, S-এর সর্বোচ্চ দৈর্ঘ্য হবে 10000।
    
    আউটপুট
    
    প্রোগ্রামটির আউটপুটে S-এ মোট কতগুলো শব্দ আছে সেটি গণনা করে দেখাতে হবে। আউটপুটে "=" চিহ্নের আগে এবং পরে একটি করে স্পেস দিতে হবে।
    
    স্যাম্পল ইনপুট আউটপুট
    স্যাম্পল #1
    ইনপুট
    3
    Hello World
    Dhaka is the capital of Bangladesh
    It is the winter of discontent
    আউটপুট
    Count = 2
    Count = 6
    Count = 6

---
+++ প্রবলেম - 19[৫২ সমস্যা বই] শব্দ গণনা-১.cpp
@@ -0,0 +1,26 @@
+//www.facebook.com/jeetrobin
+//jeet sharma
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() 
+{ 
+	string str;
+	
+	int n,cnt=0;
+	cin>>n;
+	getchar();
+	for(int i=0;i<n;i++){
+		getline(cin,str);			
+		
+			for(int i=0;i<str.length();i++){
+				if(str[i] ==' ')
+					cnt++;			
+			}	
+			
+				cout<<"Count = "<<cnt+1<<endl;	
+				cnt=0;
+	}
+
+	return 0; 
+}
