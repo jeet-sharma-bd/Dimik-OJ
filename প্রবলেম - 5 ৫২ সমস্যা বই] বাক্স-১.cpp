06595950d0816d560bb34f16852d353f8d3bb88f
Sree Prosenzit Kumar Sharma <79397907+jeet-sharma-bd@users.noreply.github.com>
2021-03-13 00:43:46 -0800

    প্রবলেম - 5 [৫২ সমস্যা বই] বাক্স-১
    তোমার হাতে যথেষ্ট পরিমাণে কাজকর্ম নেই দেখে তোমাকে একটি বাক্স আঁকার কাজ দেওয়া হলো। আসলে ব্যপারটি তেমন কিছু কঠিন নয়, তোমাকে বর্গের একটি বাহুর দৈর্ঘ্য বলা হবে আর তুমি চট করে * অক্ষরটি ব্যবহার করে ওই বর্গটি এঁকে ফেলবে। বর্গের কেবল বাহু আঁকলেই হবে না, ভিতরের ঘরগুলোও * অক্ষরটি দিয়ে পূর্ণ করে দিতে হবে। যেহেতু তুমি প্রোগ্রামিং শেখা শুরু করেছ এবং লুপ পর্যন্ত শিখে ফেলেছ, তাই তুমি কাজটি করবে একটি প্রোগ্রাম লিখে।
    
    ইনপুট
    
    প্রথম লাইনে একটি সংখ্যা T (1<=T<=25) থাকবে এবং তারপর T-সংখ্যক লাইন থাকবে। প্রতিটি লাইনে একটি করে সংখ্যা (N) থাকবে যার মান 1 থেকে 80-এর মধ্যে হবে।
    
    আউটপুট
    
    প্রতিটি N-এর জন্য N x N বর্গ আঁকতে হবে। পুরো বর্গটি * দিয়ে পূর্ণ করে দিতে হবে। প্রতিটি বর্গ একটি ফাঁকা লাইন দিয়ে পৃথক করে দিতে হবে। পৃথক করার কাজে ব্যাতিত অন্য কোথাও অতিরিক্ত ফাঁকা লাইন বা স্পেস রাখা যাবে না।
    
    স্যাম্পল ইনপুট আউটপুট
    স্যাম্পল #1
    ইনপুট
    3
    1
    3
    5
    আউটপুট
    *
    
    ***
    ***
    ***
    
    *****
    *****
    *****
    *****
    *****

---
+++ প্রবলেম - 5 ৫২ সমস্যা বই] বাক্স-১.cpp
@@ -0,0 +1,22 @@
+//www.facebook.com/jeetrobin
+//jeet sharma
+#include<bits/stdc++.h>
+using namespace std; 
+int main() 
+{ 
+	int n,a,j;
+	cin>>n;
+	while(n--){
+		cin>>a;
+		
+			for( j=0;j<a;j++){
+				for(int k=0;k<a;k++){
+					cout<<"*";
+				}
+				cout<<"\n";
+			}
+			if(n>0)cout<<"\n";
+		
+	}
+	
+}
